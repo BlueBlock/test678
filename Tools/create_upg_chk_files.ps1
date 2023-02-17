2f4b027ac5b6850d467691f2ccd94633c9301eab
BlueBlock <theblueblock@protonmail.com>
2023-02-17 16:18:56 -0500

    s

--- Tools/create_upg_chk_files.ps1
+++ Tools/create_upg_chk_files.ps1
@@ -85,63 +85,6 @@ function Resolve-UpdateCheckFileName {
     Write-Output $fileName
 }
 
-# "name": "v1.76.20",
-# 		"published_at": "2019-04-12T14:10:45Z",
-# 		"html_url": "https://github.com/mRemoteNG/mRemoteNG/releases/tag/v1.76.20",
-# 		"assets": {
-# 			"installer": {
-# 				"browser_download_url": "https://github.com/mRemoteNG/mRemoteNG/releases/download/v1.76.20/mRemoteNG-Installer-1.76.20.24615.msi",
-# 				"checksum": "AE7406070F1B4C328C716356A6E1DE3CBA0EAEEAA8F0F490C82073BA511968CF97583D0136B38D69C15EA5C1EF0C41F74A974A7200D13099522867FF6B387338",
-# 				"size": 43593728
-
-function New-MsiUpdateFileContent2 {
-    param (
-        [System.IO.FileInfo]
-        [Parameter(Mandatory=$true)]
-        $MsiFile,
-
-        [string]
-        [Parameter(Mandatory=$true)]
-        $TagName
-    )
-    
-    $version = $MsiFile.BaseName -replace "[a-zA-Z-]*"
-    $certThumbprint = (Get-AuthenticodeSignature -FilePath $MsiFile).SignerCertificate.Thumbprint
-    $hash = Get-FileHash -Algorithm SHA512 $MsiFile | % { $_.Hash }
-
-    $fileContents = `
-"Version: $version
-dURL: https://github.com/mRemoteNG/mRemoteNG/releases/download/$TagName/$($MsiFile.Name)
-clURL: https://raw.githubusercontent.com/mRemoteNG/mRemoteNG/$TagName/CHANGELOG.md
-CertificateThumbprint: $certThumbprint
-Checksum: $hash"
-    Write-Output $fileContents
-}
-
-
-function New-ZipUpdateFileContent2 {
-    param (
-        [System.IO.FileInfo]
-        [Parameter(Mandatory=$true)]
-        $ZipFile,
-
-        [string]
-        [Parameter(Mandatory=$true)]
-        $TagName
-    )
-    
-    $version = $ZipFile.BaseName -replace "[a-zA-Z-]*"
-    $hash = Get-FileHash -Algorithm SHA512 $ZipFile | % { $_.Hash }
-
-    $fileContents = `
-"Version: $version
-dURL: https://github.com/mRemoteNG/mRemoteNG/releases/download/$TagName/$($ZipFile.Name)
-clURL: https://raw.githubusercontent.com/mRemoteNG/mRemoteNG/$TagName/CHANGELOG.TXT
-Checksum: $hash"
-    Write-Output $fileContents
-}
-
-
 $body = @'
 {
 	"stable": {
@@ -218,18 +161,14 @@ if ($env:APPVEYOR_PROJECT_NAME -match "(Nightly)") {
 $buildFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\mRemoteNG\bin\x64\Release" -Resolve -ErrorAction Ignore
 
 
-
-$msiFile = Get-ChildItem -Path "$buildFolder\*.msi" | Sort-Object LastWriteTime | Select-Object -last 1
-$zipFile = Get-ChildItem -Path "$releaseFolder\*.zip" -Exclude "*-symbols-*.zip" | Sort-Object LastWriteTime | Select-Object -last 1
-Write-Output "---------------"
-Write-Output $ProjectName
-Write-Output "v$TagName"
-Write-Output (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-Write-Output "https://github.com/mRemoteNG/mRemoteNG/releases/tag/v$TagName"
-Write-Output "dURL: https://github.com/mRemoteNG/mRemoteNG/releases/download/v$TagName/$($zipFile.Name)"
-Write-Output "clURL: https://raw.githubusercontent.com/mRemoteNG/mRemoteNG/v$TagName/CHANGELOG.md"
-Write-Output "---------------"
-
+# "name": "v1.76.20",
+# 		"published_at": "2019-04-12T14:10:45Z",
+# 		"html_url": "https://github.com/mRemoteNG/mRemoteNG/releases/tag/v1.76.20",
+# 		"assets": {
+# 			"installer": {
+# 				"browser_download_url": "https://github.com/mRemoteNG/mRemoteNG/releases/download/v1.76.20/mRemoteNG-Installer-1.76.20.24615.msi",
+# 				"checksum": "AE7406070F1B4C328C716356A6E1DE3CBA0EAEEAA8F0F490C82073BA511968CF97583D0136B38D69C15EA5C1EF0C41F74A974A7200D13099522867FF6B387338",
+# 				"size": 43593728
 
 
 if ($UpdateChannel -ne "" -and $buildFolder -ne "") {
@@ -248,7 +187,16 @@ if ($UpdateChannel -ne "" -and $buildFolder -ne "") {
     $zipFile = Get-ChildItem -Path "$releaseFolder\*.zip" -Exclude "*-symbols-*.zip" | Sort-Object LastWriteTime | Select-Object -last 1
     if (![string]::IsNullOrEmpty($zipFile)) {
 
+        Write-Output "---------------"
+        Write-Output $ProjectName
+        Write-Output "v$TagName"
+        Write-Output (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+        Write-Output "https://github.com/mRemoteNG/mRemoteNG/releases/tag/v$TagName"
+        Write-Output "dURL: https://github.com/mRemoteNG/mRemoteNG/releases/download/v$TagName/$($zipFile.Name)"
+        Write-Output "clURL: https://raw.githubusercontent.com/mRemoteNG/mRemoteNG/v$TagName/CHANGELOG.md"
+        Write-Output "---------------"
 
+        
         # $pathToJson = "C:\projects\mRemoteNG.github.io\_data\releases.json"        
         # $pathToNewJson = "C:\projects\releasesNew.json"
         # $a = Get-Content $pathToJson | ConvertFrom-Json
@@ -269,9 +217,7 @@ if ($UpdateChannel -ne "" -and $buildFolder -ne "") {
         # Get-Content $a
         #Get-Content $pathToNewJson
 
-        
 
-        $zipUpdateContents2 = New-ZipUpdateFileContent2 -ZipFile $zipFile -TagName $TagName
         $zipUpdateContents = New-ZipUpdateFileContent -ZipFile $zipFile -TagName $TagName
         $zipUpdateFileName = Resolve-UpdateCheckFileName -UpdateChannel $UpdateChannel -Type Portable
         Write-Output "`n`nZip Update Check File Contents ($zipUpdateFileName)`n------------------------------"
