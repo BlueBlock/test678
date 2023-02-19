7cfd0178ebd70d84b568dfad4d319140fd9ed950
BlueBlock <theblueblock@protonmail.com>
2023-02-19 13:31:16 -0500

    3

--- Tools/postbuild_mremoteng.ps1
+++ Tools/postbuild_mremoteng.ps1
@@ -53,6 +53,8 @@ Format-Table -AutoSize -Wrap -InputObject @{
 # }
 ###
 
+write-host "CURRENT_GITHUB_USER = $CURRENT_GITHUB_USER"
+
 & "$PSScriptRoot\set_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
 & "$PSScriptRoot\verify_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
 & "$PSScriptRoot\tidy_files_for_release.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
