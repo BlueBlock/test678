9408f208d499c84defb7192fe9905e540be782bc
BlueBlock <theblueblock@protonmail.com>
2023-02-19 06:03:27 -0500

    7

--- Tools/create_website_release_json_file.ps1
+++ Tools/create_website_release_json_file.ps1
@@ -33,7 +33,7 @@ if ($env:APPVEYOR_PROJECT_NAME -match "(Nightly)") {
     $UpdateChannel = ""
 }
 
-Set-GitHubContent -OwnerName blueblock -RepositoryName mRemoteNG.github.io  -Path README4.md -CommitMessage 'Adding README.md' -Content '# README' -BranchName main
+Set-GitHubContent -OwnerName blueblock -RepositoryName mRemoteNG.github.io  -Path README4.md -CommitMessage 'Adding README.md' -Content '# README 4' -BranchName main
 
 
 $buildFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\mRemoteNG\bin\x64\Release" -Resolve -ErrorAction Ignore
