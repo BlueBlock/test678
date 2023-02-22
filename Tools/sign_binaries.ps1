6f4152df4da2da3024f0f29a03dc9692325e968e
BlueBlock <theblueblock@protonmail.com>
2023-02-22 13:22:15 -0500

    6

--- Tools/sign_binaries.ps1
+++ Tools/sign_binaries.ps1
@@ -30,6 +30,17 @@ $timeserver = "http://timestamp.verisign.com/scripts/timstamp.dll"
 
 Write-Output "1"
 
+# $current_path = Get-Location
+# Write-Output "aaa: 0a"
+# Set-Location "$Env:APPVEYOR_BUILD_FOLDER\..\"
+# Write-Output "aaa: 0b"
+# #Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/appveyor/secure-file/master/install.ps1'))
+# #Invoke-WebRequest "https://raw.githubusercontent.com/appveyor/secure-file/master/install.ps1" | Invoke-Expression
+# Write-Output "aaa: 0c"
+# Set-Location $current_path
+# Write-Output "aaa: 0d"
+
+
 # decrypt cert if on appveyor
 if ($ConfigurationName -match "Release" -And ("$($Env:CERT_PATH).enc")) {
 	Write-Output "2"
@@ -37,12 +48,13 @@ if ($ConfigurationName -match "Release" -And ("$($Env:CERT_PATH).enc")) {
 		Write-Output "3"
 		Write-Output $Env:APPVEYOR_BUILD_FOLDER
 		Write-Output "Decrypting cert.."
+		$current_path = Get-Location
 		Write-Output "$Env:APPVEYOR_BUILD_FOLDER\..\appveyor-tools\secure-file.exe"
 		Write-Output "$($Env:CERT_PATH).enc"
-		Write-Output "$Env:CERT_DECRYPT_PWD"
-		"$Env:APPVEYOR_BUILD_FOLDER\..\appveyor-tools\secure-file.exe -decrypt '$($Env:CERT_PATH).enc' -secret '$Env:CERT_DECRYPT_PWD' "
+		"$Env:APPVEYOR_BUILD_FOLDER\..\appveyor-tools\secure-file.exe -decrypt '$Env:APPVEYOR_BUILD_FOLDER\$($Env:CERT_PATH).enc' -secret '$Env:CERT_DECRYPT_PWD' "
 		#$Env:APPVEYOR_BUILD_FOLDER\..\appveyor-tools\secure-file.exe -decrypt "$($Env:CERT_PATH).enc" -secret "$Env:CERT_DECRYPT_PWD"
 		$CertificatePath = Join-Path -Path $SolutionDir -ChildPath $CertificatePath
+		Set-Location $current_path
 	}
 }
 
