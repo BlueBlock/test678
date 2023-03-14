ae8be8a428c060f6b89d180be719d1a4aa6fa2bb
BlueBlock <theblueblock@protonmail.com>
2023-03-14 05:16:25 -0400

    1

--- Tools/rename_and_copy_installer.ps1
+++ Tools/rename_and_copy_installer.ps1
@@ -10,11 +10,13 @@ Write-Output "===== Beginning rename_and_copy_installer.ps1 ====="
 $targetVersionedFile = "$SolutionDir\mRemoteNG\bin\x64\$BuildConfiguration\mRemoteNG.exe"
 #$fileversion = &"$SolutionDir\Tools\exes\sigcheck.exe" /accepteula -q -n $targetVersionedFile
 $prodversion = ((Get-Item -Path $targetVersionedFile).VersionInfo | Select-Object -Property ProductVersion)."ProductVersion"
+$fileversion = ((Get-Item -Path $targetVersionedFile).VersionInfo | Select-Object -Property FileVersion)."FileVersion"
 $src = $SolutionDir + "mRemoteNGInstaller\Installer\bin\x64\$BuildConfiguration\en-US\mRemoteNG-Installer.msi"
 $dst = $SolutionDir + "mRemoteNG\bin\x64\$BuildConfiguration\mRemoteNG-Installer-" + $prodversion + ".msi"
 
 # Copy file
 Write-Host $prodversion
+Write-Host $fileversion
 Write-Host $src
 Write-Host $dst
 Copy-Item $src -Destination $dst -Force
