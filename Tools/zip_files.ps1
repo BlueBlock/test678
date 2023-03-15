c2df868b76d1191b9ef9173441a5588b8512cd78
BlueBlock <theblueblock@protonmail.com>
2023-03-15 16:34:28 -0400

    5

--- Tools/zip_files.ps1
+++ Tools/zip_files.ps1
@@ -13,13 +13,26 @@
 )
 
 Write-Output ""
-Write-Output "===== Beginning $($PSCmdlet.MyInvocation.MyCommand) ====="
+Write-Output "===== Begin $($PSCmdlet.MyInvocation.MyCommand) ====="
 
 $ConfigurationName = $ConfigurationName.Trim()
 Write-Output "Config Name (trimmed): '$($ConfigurationName)'"
 $exe = Join-Path -Path $TargetDir -ChildPath $TargetFileName
 $version = ((Get-Item -Path $exe).VersionInfo | Select-Object -Property ProductVersion)."ProductVersion"
 Write-Output "Version is $($version)"
+
+# determine update channel
+if ($env:APPVEYOR_PROJECT_NAME -match "(Nightly)") {
+    write-host "UpdateChannel = Nightly"
+    $ModifiedVersion = "$version-NB"
+} elseif ($env:APPVEYOR_PROJECT_NAME -match "(Preview)") {
+    write-host "UpdateChannel = Preview"
+    $ModifiedVersion = "$version-PB"
+} elseif ($env:APPVEYOR_PROJECT_NAME -match "(Stable)") {
+    write-host "UpdateChannel = Stable"
+    $ModifiedVersion = $version
+} else {
+}
 
 # Fix for AppVeyor
 if(!([string]::IsNullOrEmpty($Env:APPVEYOR_BUILD_FOLDER))) {
@@ -35,21 +48,21 @@ if ($ConfigurationName -match "Release") {
     if ($ConfigurationName -match "Portable") {
         $zipFilePrefix = "mRemoteNG-Portable-symbols"
     } else {
-        $zipFilePrefix = "mRemoteNG-symbols"
+        $zipFilePrefix = "mRemoteNG-Installer-symbols"
     }
 
     $debugFile = Join-Path -Path $TargetDir -ChildPath "mRemoteNG.pdb"
 
     # AppVeyor build
     if(!([string]::IsNullOrEmpty($Env:APPVEYOR_BUILD_FOLDER))) {
-        $outputZipPath = Join-Path -Path $SolutionDir -ChildPath "Release\$zipFilePrefix-$($version).zip"
+        $outputZipPath = Join-Path -Path $SolutionDir -ChildPath "Release\$zipFilePrefix-$($ModifiedVersion).zip"
         7z a $outputZipPath $debugFile
     }
     # Local build
     else {
 		if (!(Test-Path -Path $debugFile -PathType Leaf))
 		{
-			$outputZipPath = "$($SolutionDir)Release\$zipFilePrefix-$($version).zip"
+			$outputZipPath = "$($SolutionDir)Release\$zipFilePrefix-$($ModifiedVersion).zip"
 			Compress-Archive $debugFile $outputZipPath -Force
 		} else {
 			write-host "File do not exist:" $debugFile", nothing to compress"
@@ -65,14 +78,14 @@ if ($ConfigurationName -eq "Release Portable") {
 
     # AppVeyor build
     if(!([string]::IsNullOrEmpty($Env:APPVEYOR_BUILD_FOLDER))) {
-        $outputZipPath = Join-Path -Path $SolutionDir -ChildPath "Release\mRemoteNG-Portable-$($version).zip"
+        $outputZipPath = Join-Path -Path $SolutionDir -ChildPath "Release\mRemoteNG-Portable-$($ModifiedVersion).zip"
         7z a -bt -bd -bb1 -mx=9 -tzip -y -r $outputZipPath $TargetDir\*
     }
     # Local build
     else {
 		if ($Source)
 		{
-			$outputZipPath="$($SolutionDir)\Release\mRemoteNG-Portable-$($version).zip"
+			$outputZipPath="$($SolutionDir)\Release\mRemoteNG-Portable-$($ModifiedVersion).zip"
 			Compress-Archive $Source $outputZipPath -Force
 		} else {
 			write-host "File do not exist:" $Source", nothing to compress"
