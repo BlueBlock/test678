4703ac924deec742e19a7641438eff0225484933
BlueBlock <theblueblock@protonmail.com>
2023-03-16 12:47:35 -0400

    1

--- ExternalConnectors/PuttyKeyFileGenerator.cs
+++ ExternalConnectors/PuttyKeyFileGenerator.cs
@@ -19,68 +19,73 @@ public class PuttyKeyFileGenerator
     public static string ToPuttyPrivateKey(RSACryptoServiceProvider cryptoServiceProvider, string Comment = "imported-openssh-key")
     {
         var publicParameters = cryptoServiceProvider.ExportParameters(false);
-        byte[] publicBuffer = new byte[3 + keyType.Length + GetPrefixSize(publicParameters.Exponent) + publicParameters.Exponent.Length +
-                GetPrefixSize(publicParameters.Modulus) + publicParameters.Modulus.Length + 1];
-
-        using (var bw = new BinaryWriter(new MemoryStream(publicBuffer)))
+        if (publicParameters.Modulus != null)
         {
-            bw.Write(new byte[] { 0x00, 0x00, 0x00 });
-            bw.Write(keyType);
-            PutPrefixed(bw, publicParameters.Exponent, CheckIsNeddPadding(publicParameters.Exponent));
-            PutPrefixed(bw, publicParameters.Modulus, CheckIsNeddPadding(publicParameters.Modulus));
+            byte[] publicBuffer = new byte[3 + keyType.Length + GetPrefixSize(publicParameters.Exponent) + publicParameters.Exponent.Length +
+                                           GetPrefixSize(publicParameters.Modulus) + publicParameters.Modulus.Length + 1];
+
+            using (var bw = new BinaryWriter(new MemoryStream(publicBuffer)))
+            {
+                bw.Write(new byte[] { 0x00, 0x00, 0x00 });
+                bw.Write(keyType);
+                PutPrefixed(bw, publicParameters.Exponent, CheckIsNeddPadding(publicParameters.Exponent));
+                PutPrefixed(bw, publicParameters.Modulus, CheckIsNeddPadding(publicParameters.Modulus));
+            }
+            var publicBlob = System.Convert.ToBase64String(publicBuffer);
+
+            var privateParameters = cryptoServiceProvider.ExportParameters(true);
+            byte[] privateBuffer = new byte[paddedPrefixSize + privateParameters.D.Length + paddedPrefixSize + privateParameters.P.Length + paddedPrefixSize + privateParameters.Q.Length + paddedPrefixSize + privateParameters.InverseQ.Length];
+
+            using (var bw = new BinaryWriter(new MemoryStream(privateBuffer)))
+            {
+                PutPrefixed(bw, privateParameters.D, true);
+                PutPrefixed(bw, privateParameters.P, true);
+                PutPrefixed(bw, privateParameters.Q, true);
+                PutPrefixed(bw, privateParameters.InverseQ, true);
+            }
+            var privateBlob = System.Convert.ToBase64String(privateBuffer);
+
+            HMACSHA1 hmacSha1 = new HMACSHA1(SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes("putty-private-key-file-mac-key")));
+            //byte[] bytesToHash = new byte[4 + 7 + 4 + 4 + 4 + Comment.Length + 4 + publicBuffer.Length + 4 + privateBuffer.Length];
+            byte[] bytesToHash = new byte[prefixSize + keyType.Length + prefixSize + encryptionType.Length + prefixSize + Comment.Length +
+                                          prefixSize + publicBuffer.Length + prefixSize + privateBuffer.Length];
+
+            using (var bw = new BinaryWriter(new MemoryStream(bytesToHash)))
+            {
+                PutPrefixed(bw, Encoding.ASCII.GetBytes("ssh-rsa"));
+                PutPrefixed(bw, Encoding.ASCII.GetBytes("none"));
+                PutPrefixed(bw, Encoding.ASCII.GetBytes(Comment));
+                PutPrefixed(bw, publicBuffer);
+                PutPrefixed(bw, privateBuffer);
+            }
+
+            var hash = string.Join("", hmacSha1.ComputeHash(bytesToHash).Select(x => $"{x:x2}"));
+
+            var sb = new StringBuilder();
+            sb.AppendLine("PuTTY-User-Key-File-2: " + keyType);
+            sb.AppendLine("Encryption: " + encryptionType);
+            sb.AppendLine("Comment: " + Comment);
+
+            var publicLines = SpliceText(publicBlob, lineLength);
+            sb.AppendLine("Public-Lines: " + publicLines.Length);
+            foreach (var line in publicLines)
+            {
+                sb.AppendLine(line);
+            }
+
+            var privateLines = SpliceText(privateBlob, lineLength);
+            sb.AppendLine("Private-Lines: " + privateLines.Length);
+            foreach (var line in privateLines)
+            {
+                sb.AppendLine(line);
+            }
+
+            sb.AppendLine("Private-MAC: " + hash);
+
+            return sb.ToString();
         }
-        var publicBlob = System.Convert.ToBase64String(publicBuffer);
 
-        var privateParameters = cryptoServiceProvider.ExportParameters(true);
-        byte[] privateBuffer = new byte[paddedPrefixSize + privateParameters.D.Length + paddedPrefixSize + privateParameters.P.Length + paddedPrefixSize + privateParameters.Q.Length + paddedPrefixSize + privateParameters.InverseQ.Length];
-
-        using (var bw = new BinaryWriter(new MemoryStream(privateBuffer)))
-        {
-            PutPrefixed(bw, privateParameters.D, true);
-            PutPrefixed(bw, privateParameters.P, true);
-            PutPrefixed(bw, privateParameters.Q, true);
-            PutPrefixed(bw, privateParameters.InverseQ, true);
-        }
-        var privateBlob = System.Convert.ToBase64String(privateBuffer);
-
-        HMACSHA1 hmacsha1 = new HMACSHA1(new SHA1CryptoServiceProvider().ComputeHash(Encoding.ASCII.GetBytes("putty-private-key-file-mac-key")));
-        //byte[] bytesToHash = new byte[4 + 7 + 4 + 4 + 4 + Comment.Length + 4 + publicBuffer.Length + 4 + privateBuffer.Length];
-        byte[] bytesToHash = new byte[prefixSize + keyType.Length + prefixSize + encryptionType.Length + prefixSize + Comment.Length +
-                                      prefixSize + publicBuffer.Length + prefixSize + privateBuffer.Length];
-
-        using (var bw = new BinaryWriter(new MemoryStream(bytesToHash)))
-        {
-            PutPrefixed(bw, Encoding.ASCII.GetBytes("ssh-rsa"));
-            PutPrefixed(bw, Encoding.ASCII.GetBytes("none"));
-            PutPrefixed(bw, Encoding.ASCII.GetBytes(Comment));
-            PutPrefixed(bw, publicBuffer);
-            PutPrefixed(bw, privateBuffer);
-        }
-
-        var hash = string.Join("", hmacsha1.ComputeHash(bytesToHash).Select(x => string.Format("{0:x2}", x)));
-
-        var sb = new StringBuilder();
-        sb.AppendLine("PuTTY-User-Key-File-2: " + keyType);
-        sb.AppendLine("Encryption: " + encryptionType);
-        sb.AppendLine("Comment: " + Comment);
-
-        var publicLines = SpliceText(publicBlob, lineLength);
-        sb.AppendLine("Public-Lines: " + publicLines.Length);
-        foreach (var line in publicLines)
-        {
-            sb.AppendLine(line);
-        }
-
-        var privateLines = SpliceText(privateBlob, lineLength);
-        sb.AppendLine("Private-Lines: " + privateLines.Length);
-        foreach (var line in privateLines)
-        {
-            sb.AppendLine(line);
-        }
-
-        sb.AppendLine("Private-MAC: " + hash);
-
-        return sb.ToString();
+        return string.Empty;
     }
     private static void PutPrefixed(BinaryWriter bw, byte[] bytes, bool addLeadingNull = false)
     {
