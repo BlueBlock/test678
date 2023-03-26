bc9531c8b1f2b922898bebce4be61f8332781e2c
BlueBlock <theblueblock@protonmail.com>
2023-03-26 10:18:10 -0400

    6

--- Tools/postbuild_installer.ps1
+++ Tools/postbuild_installer.ps1
@@ -58,5 +58,7 @@ $env:postbuild_installer_executed="true"
 [Environment]::SetEnvironmentVariable("postbuild_installer_executed", "true", [System.EnvironmentVariableTarget]::Session)
 [Environment]::SetEnvironmentVariable("postbuild_installer_executed", "true", [System.EnvironmentVariableTarget]::User)
 
+Set-Item -Path Env:postbuild_installer_executed -Value ($Env:postbuild_installer_executed + ";true2")
+
 Write-Output "End mRemoteNG Installer Post Build"
 Write-Output ""
