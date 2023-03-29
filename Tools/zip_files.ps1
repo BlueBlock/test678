9e00c73a9539fedfc6a31538029f3cb178528c23
BlueBlock <theblueblock@protonmail.com>
2023-03-29 14:31:50 -0400

    6

--- Tools/zip_files.ps1
+++ Tools/zip_files.ps1
@@ -22,8 +22,11 @@ Write-Output "Config Name (trimmed): '$($ConfigurationName)'"
 $exe = Join-Path -Path $TargetDir -ChildPath $TargetFileName
 $version = ((Get-Item -Path $exe).VersionInfo | Select-Object -Property ProductVersion)."ProductVersion"
 Write-Output "*Version is $($version)"
+
+Write-Output "*FileVersion: $($(Select-Object -Property ProductVersion).FileVersion)"
 Write-Output "*VersionInfo: $($(Get-Item -Path $exe).VersionInfo)"
 Write-Output "*ProductVersion: $($(Select-Object -Property ProductVersion).ProductVersion)"
+
 
 # determine update channel
 if ($env:APPVEYOR_PROJECT_NAME -match "(Nightly)") {
