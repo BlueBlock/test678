9079eef2c8da46f56e1d17551f06e9fc1e21cccc
BlueBlock <theblueblock@protonmail.com>
2023-03-28 23:31:43 -0400

    1

--- Tools/postbuild_installer.ps1
+++ Tools/postbuild_installer.ps1
@@ -46,7 +46,7 @@ Format-Table -AutoSize -Wrap -InputObject @{
 
 & "$PSScriptRoot\rename_and_copy_installer.ps1" -SolutionDir $SolutionDir -BuildConfiguration $ConfigurationName.Trim()
 
-Write-Output "Test-Path('C:\projects\mremoteng-nb7nb\mRemoteNGInstaller\Installer\Installer.wixproj')"
+Write-Output Test-Path("C:\projects\mremoteng-nb7nb\mRemoteNGInstaller\Installer\Installer.wixproj")
 
 if ( ($ConfigurationName.ToUpper() -match "RELEASE") -and (($env:APPVEYOR_PROJECT_NAME).ToUpper() -notcontains "(CI)") -and (![string]::IsNullOrEmpty($env:WEBSITE_TARGET_OWNER)) -and (![string]::IsNullOrEmpty($env:WEBSITE_TARGET_REPOSITORY)) ) {
 
