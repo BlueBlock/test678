719c05fe31e7706d883420af25ac25efa2be11ce
BlueBlock <theblueblock@protonmail.com>
2023-03-30 12:08:53 -0400

    6

--- Tools/rename_and_copy_installer.ps1
+++ Tools/rename_and_copy_installer.ps1
@@ -49,10 +49,10 @@ try
 }
 catch
 {
-    Write-Host "        [Failure!5]" -ForegroundColor red
+    Write-Host "        [Failure!6]" -ForegroundColor red
     Write-Output $Error[0]
     $PSCmdlet.ThrowTerminatingError()
-    return 1
+    return
 }
 
 
